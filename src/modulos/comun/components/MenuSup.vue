c7f7b7dc60392552086165b2de69fecb267d752c
Yoana24 <yoanapla@gmail.com>
2025-04-03 17:40:44 +0200

    Menu DaisyUI

---
+++ src/modulos/comun/components/MenuSup.vue
@@ -0,0 +1,10 @@
+
+<template>
+
+    <div class="flex items-center justify-between p-4 bg-base-200">
+        <h2 class="text-lg font-bold text-pink-700">Notion App</h2>
+    </div>
+
+    
+
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
+import ProjectsLayout from '@/modulos/projects/layouts/ProjectsLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout
+
+    }
    
   ],
 })
